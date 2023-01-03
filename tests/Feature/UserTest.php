951b34ee274244e2a2b29cc6799a3d9c4c7353b6
Khien Pham <phamkhien@hotmail.com>
2023-01-03 13:46:01 +0700

    C9 Fix CI & code coverage (#9)

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testCreateANewUser()
+    {
+        $response = $this->post('/api/users/create', ['name' => 'Khien', 'email' => rand(1, 10000) . "_test@khien.com", 'password' => 123456])
+            ->assertJson([
+                'success' => true,
+                'code' => 201,
+                'message' => 'Created successfully!',
+                'data' => [
+                    'name' => 'Khien',
+                ]
+            ])->assertJsonStructure([
+                'success',
+                'code',
+                'message',
+                'data' => ['id', 'name', 'email']
+            ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testCreateAnExistingUser()
+    {
+        $this->post('/api/users/create', ['name' => 'Khien', 'email' =>  "test@khien.com", 'password' => 123456])
+            ->assertJson(['success' => true]);
+
+        $response = $this->post('/api/users/create', ['name' => 'Khien', 'email' =>  "test@khien.com", 'password' => 123456])
+            ->assertJson([
+                'success' => false,
+                'code' => 409,
+                'message' => 'User email already exists',
+                'data' => [
+                ]
+            ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testCreateTokenWithInvalidData()
+    {
+        $this->post('/api/users/create', ['name' => 'Khien', 'email' =>  "test@khien.com", 'password' => 123456])
+            ->assertJson(['success' => true]);
+
+        $response = $this->post('/api/users/token', ['email' =>  "test@khien.com", 'password' => 12333456, 'device_name' => 'aaa'])
+            ->assertJson([
+                'success' => false,
+                'message' => 'The provided credentials are incorrect.',
+            ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testCreateTokenSuccessfully()
+    {
+        $this->post('/api/users/create', ['name' => 'Khien', 'email' =>  "test@khien.com", 'password' => 123456])
+            ->assertJson(['success' => true]);
+
+        $response = $this->post('/api/users/token', ['email' =>  "test@khien.com", 'password' => 123456, 'device_name' => 'aaa'])
+            ->assertJson([
+                'success' => true,
+                'code' => 201,
+                'message' => 'Created successfully!',
+            ]);
+
+        $response->assertStatus(200);
+    }
+}
